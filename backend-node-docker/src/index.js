1e100831715ab769565192a43d05acc924ff200d
bpgualotuna <brayan.gualotuna09@gmail.com>
2026-04-09 08:42:18 -0500

    reto 4

--- backend-node-docker/src/index.js
+++ backend-node-docker/src/index.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
+
+
+
 const express = require("express");
 const app = express();
+const port = process.env.port || 3000;
+const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
 
@@ -9,9 +15,11 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("API Backend Funcionando");
 });
+console.log("Clave secreta: ", jwtSecret);
+console.log("Puerto: ", port);
 
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server escuchando en el puerto: ${PORT}`);
+
+app.listen(port, () => {
+    console.log(`Server escuchando en el puerto: ${port}`);
 });
